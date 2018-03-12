03828ecbb12f2e0d1f9a027a3e8e07b32fd58e07
andrewthenintendrone <andrewfitzpatrick@y7mail.com>
2018-03-13 10:27:15 +1100

    done

--- PhysicsApp/source/PhysicsApp.cpp
+++ PhysicsApp/source/PhysicsApp.cpp
@@ -33,36 +33,18 @@ bool PhysicsApp::startup()
 	m_font = new aie::Font("./font/consolas.ttf", 32);
 
 	m_physicsScene = new PhysicsScene();
-	m_physicsScene->setGravity(glm::vec2(0, -10));
+	m_physicsScene->setGravity(glm::vec2(0, -100));
 	m_physicsScene->setTimeStep(0.01f);
 
 	Plane* plane1 = new Plane();
 	plane1->setNormal(1, 2);
-	plane1->setDistance(-50);
+	plane1->setDistance(300);
 	m_physicsScene->addActor(plane1);
 
 	Plane* plane2 = new Plane();
 	plane2->setNormal(1, -2);
-	plane2->setDistance(50);
+	plane2->setDistance(300);
 	m_physicsScene->addActor(plane2);
-
-	for (int x = 0; x < 5; x++)
-	{
-		for (int y = 0; y < 5; y++)
-		{
-			glm::vec2 position(-25 + x * 12.5f, y * 10);
-			glm::vec4 randomColor(rand() % 256 / 255.0f, rand() % 256 / 255.0f, rand() % 256 / 255.0f, 1.0f);
-
-			Sphere* ball = new Sphere();
-			ball->setPosition(position);
-			ball->setMass(1);
-			ball->setRadius(5);
-			ball->setElasticity(0.3f);
-			ball->setColor(randomColor);
-			ball->calculateMoment();
-			m_physicsScene->addActor(ball);
-		}
-	}
 	
 	int numBoxes = 16;
 
@@ -76,9 +58,12 @@ bool PhysicsApp::startup()
 		float cs = std::cosf(theta);
 
 		glm::vec2 position(sn, -cs);
+		position *= 200;
+		position.x += getWindowWidth() * 0.5f;
+		position.y += getWindowHeight() * 0.5f;
 
-		position *= 40;
-
+		box->setHeight(10);
+		box->setWidth(10);
 		box->setPosition(position);
 		box->setMass(1);
 		box->setKinematic(true);
@@ -101,6 +86,22 @@ void PhysicsApp::update(float deltaTime)
 	aie::Input* input = aie::Input::getInstance();
 
 	aie::Gizmos::clear();
+
+	glm::vec2 mousePos = glm::vec2(input->getMouseX(), input->getMouseY());
+
+	if (input->wasMouseButtonPressed(0))
+	{
+		glm::vec4 randomColor(rand() % 256 / 255.0f, rand() % 256 / 255.0f, rand() % 256 / 255.0f, 1.0f);
+
+		Sphere* ball = new Sphere();
+		ball->setPosition(mousePos);
+		ball->setMass(1);
+		ball->setRadius(20);
+		ball->setElasticity(0.3f);
+		ball->setColor(randomColor);
+		ball->calculateMoment();
+		m_physicsScene->addActor(ball);
+	}
 
 	m_physicsScene->update(deltaTime);
 	m_physicsScene->draw();
@@ -126,7 +127,7 @@ void PhysicsApp::draw()
 	m_2dRenderer->begin();
 
 	float aspectRatio = 16 / 9.f;
-	aie::Gizmos::draw2D(glm::ortho<float>(-100, 100, -100 / aspectRatio, 100 / aspectRatio, -1.0f, 1.0f));
+	aie::Gizmos::draw2D(glm::ortho<float>(0, getWindowWidth(), 0, getWindowHeight(), -1.0f, 1.0f));
 
 	// output some text, uses the last used colour
 	m_2dRenderer->drawText(m_font, "Press ESC to quit", 0, 0);
